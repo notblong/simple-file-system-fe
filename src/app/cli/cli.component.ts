d212683aac48ee427f0d0eda9e39b2e5524d4332
Long Pham <longpham.work23@gmail.com>
2023-05-07 14:32:02 +0700

    refactor

--- src/app/cli/cli.component.ts
+++ src/app/cli/cli.component.ts
@@ -9,63 +9,82 @@ import { FileService } from '../shared/services/files/file.service';
   styleUrls: ['./cli.component.scss'],
 })
 export class CliComponent implements OnInit, AfterViewInit {
-  @ViewChild('term', { static: true }) child: NgTerminal;
+  @ViewChild('term', { static: true }) terminal: NgTerminal;
 
-  public readonly ENTER_KEYCODE = 13;
-  public readonly BACKSPACE_KEYCODE = 8;
-  public readonly GREEN_ANSI_COLOR = '\x1b[1;32m';
-  public readonly BLUE_ANSI_COLOR = '\x1b[1;34m';
-  public readonly WHITE_ANSI_COLOR = '\x1b[37m';
-  public prefix = `${this.GREEN_ANSI_COLOR}file-system: ${this.WHITE_ANSI_COLOR}`;
-  public currentPath = '/';
-  public command = '';
+  readonly ENTER_KEYCODE = 13;
+  readonly BACKSPACE_KEYCODE = 8;
+  readonly GREEN_ANSI_COLOR = '\x1b[1;32m';
+  readonly BLUE_ANSI_COLOR = '\x1b[1;34m';
+  readonly WHITE_ANSI_COLOR = '\x1b[37m';
+  readonly supportedCommands = {
+    cd: 'cd',
+    ls: 'ls',
+    help: 'help',
+  }
+  systemName = 'file-system';
+  prefix = `${this.GREEN_ANSI_COLOR}${this.systemName} ${this.WHITE_ANSI_COLOR}`;
+  currentPath = '/';
+  command = '';
+
+  get fixedText(): number {
+    return `${this.systemName}${this.currentPath}$ `.length + 1;
+  }
 
   constructor(
     private readonly fileExplorerService: FileRestService,
     private readonly fileService: FileService,
   ) {}
 
-  public ngOnInit(): void {}
+  ngOnInit(): void {}
 
-  public ngAfterViewInit(): void {
-    if (this.child) {
-      this.child.write(`hint: type 'help' to see supported commands\r\n`);
-      this.child.write(`${this.prefix}${this.currentPath}$ `);
-    }
-
-    this.child.keyEventInput.subscribe(async (e) => {
+  public onTyping() {
+    this.terminal.keyEventInput.subscribe(async (e) => {
       const ev = e.domEvent;
       const printable = !ev.altKey && !ev.ctrlKey && !ev.metaKey;
 
       if (ev.keyCode === this.ENTER_KEYCODE) {
-        await this.execute(this.command, this.child);
+        await this.execute(this.command, this.terminal);
         this.command = '';
-        this.child.write(`\r\n${this.prefix}${this.currentPath}$ `);
+        this.terminal.write(`\r\n${this.prefix}${this.currentPath}$ `);
       } else if (ev.keyCode === this.BACKSPACE_KEYCODE) {
-        if (this.child.underlying.buffer.active.cursorX > 2) {
-          this.child.write('\b \b');
+        if (this.terminal.underlying.buffer.active.cursorX > this.fixedText) {
+          this.terminal.write('\b \b');
           this.command = this.command.substring(0, this.command.length - 1);
         }
       } else if (printable) {
-        this.child.write(e.key);
+        this.terminal.write(e.key);
         this.command = this.command.concat(e.key);
       }
     });
   }
 
+  public ngAfterViewInit(): void {
+    if (this.terminal) {
+      this.terminal.write(`hint: type 'help' to see supported commands\r\n`);
+      this.terminal.write(`${this.prefix}${this.currentPath}$ `);
+    }
+
+    this.onTyping();
+  }
+
   public async execute(command: string, child: NgTerminal) {
     command = command.replace('\r', '');
-    const args = command.split(' ');
+    const args = command.split(' '); // need to refactor to regex
     const cmd = args[0];
+    if (!this.supportedCommands[cmd]) {
+      this.terminal.write(`\r\n  ${command} - command not found`);
+      return;
+    }
+
     console.log(args);
     switch (cmd) {
       case 'ls': {
         await this.fileExplorerService.list(this.currentPath).then((resp) => {
           const items = resp.data;
           items.forEach((item) =>
-            this.child.write(`\r\n ${this.showItemInfo(item)}`)
+            this.terminal.write(`\r\n ${this.showItemInfo(item)}`)
             );
-          this.child.write(`\r\n  Total: ${items.length}`);
+          this.terminal.write(`\r\n  Total: ${items.length}`);
         });
         break;
       }
@@ -76,17 +95,13 @@ export class CliComponent implements OnInit, AfterViewInit {
           if (valid || targetPath == '/') {
             this.currentPath = targetPath;
           } else {
-            this.child.write(`\r\n  Cannot found ${targetPath}`);
+            this.terminal.write(`\r\n  Cannot found ${targetPath}`);
           }
         });
         break;
       }
       case 'help': {
         this.showHelp();
-        break;
-      }
-      default: {
-        this.child.write(`\r\n  ${command} - command not found`);
         break;
       }
     }
@@ -125,12 +140,12 @@ export class CliComponent implements OnInit, AfterViewInit {
     return this.fileService.pushToPath(clone_currentPath, cmd);
   }
 
-  public isRoot(currentPath) {
+  public isRoot(currentPath): boolean {
     return currentPath == '/';
   }
 
   public showHelp() {
-    this.child.write(`\r\n  cd FOLDER_PATH  : change current working directory/folder to the specified FOLDER`);
-    this.child.write(`\r\n  ls              : list out all items directly under a folder`);
+    this.terminal.write(`\r\n  cd FOLDER_PATH  : change current working directory/folder to the specified FOLDER`);
+    this.terminal.write(`\r\n  ls              : list out all items directly under a folder`);
   }
 }
